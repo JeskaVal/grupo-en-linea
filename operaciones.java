9c86800bffbd005d65c42cf4a94a1e9e14e35026
Jessica <jessgarcia.git@gmail.com>
2024-05-02 23:05:35 -0700

    En esta parte del proyecto agregue un metodo de suma

---
+++ operaciones.java
@@ -0,0 +1,25 @@
+public class operaciones {
+
+    private int suma; 
+    private int mult;
+    private int resta;
+    private int numero1,numero2;
+
+    operaciones(int num1, int num2){
+        this.numero1=num1;
+        this.numero2=num2;
+
+
+    }
+
+    public void sumaDdosnum(int num1, int num2){
+    
+        suma = nume1+nume2;
+
+        System.out.println("La suma de los dos numeros es: "+suma);
+    
+
+
+
+    }
+}
